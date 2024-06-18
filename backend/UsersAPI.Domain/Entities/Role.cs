63a7caf57f2a56a55f0639708155f31f42742494
Álefe Alves <alefealves@hotmail.com>
2024-06-18 16:38:59 -0400

    feat: add entidades da solucao

---
+++ backend/UsersAPI.Domain/Entities/Role.cs
@@ -0,0 +1,16 @@
+namespace UsersAPI.Domain.Entities
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+        public string? RoleName { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+
+        /// <summary>
+        /// Relacionamentos entre Entidades
+        /// </summary>
+        public List<Permission>? Permissions { get; set; }
+        public List<User>? Users { get; set; }
+    }
+}
