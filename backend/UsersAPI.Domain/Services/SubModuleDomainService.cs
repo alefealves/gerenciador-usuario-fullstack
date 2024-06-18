8c1d5090c5ace6155d7c4efaff194c73d221090a
Álefe Alves <alefealves@hotmail.com>
2024-06-18 18:08:11 -0400

    feat: add services

---
+++ backend/UsersAPI.Domain/Services/SubModuleDomainService.cs
@@ -0,0 +1,78 @@
+using UsersAPI.Domain.Entities;
+using UsersAPI.Domain.Exceptions;
+using UsersAPI.Domain.Interfaces.Repositories;
+using UsersAPI.Domain.Interfaces.Services;
+
+namespace UsersAPI.Domain.Services
+{
+  public class SubModuleDomainService : ISubModuleDomainService
+  {
+    private readonly IUnitOfWork? _unitOfWork;
+
+    public SubModuleDomainService(IUnitOfWork? unitOfWork)
+    {
+      _unitOfWork = unitOfWork;
+    }
+
+    public void Add(SubModule subModule)
+    {
+      if (GetByModuleId(subModule.ModuleId) == null)
+        throw new ModuleIsNotExistsException(subModule.ModuleId);
+
+      if (Get(subModule.SubModuleName) != null)
+        throw new SubModuleAlreadyExistsException(subModule.SubModuleName);
+
+      _unitOfWork?.SubModuleRepository.Add(subModule);
+      _unitOfWork?.SaveChanges();
+    }
+
+    public void Update(SubModule subModule)
+    {
+      _unitOfWork?.SubModuleRepository.Update(subModule);
+      _unitOfWork?.SaveChanges();
+    }
+
+    public void Delete(SubModule subModule)
+    {
+      if (_unitOfWork?.SubModuleRepository.GetByPermission(subModule.Id) != null)
+        throw new SubModuleIsNotDeleteException(subModule.SubModuleName);
+
+      _unitOfWork?.SubModuleRepository.Delete(subModule);
+      _unitOfWork?.SaveChanges();
+    }
+
+    public List<SubModule> GetAll()
+    {
+      return _unitOfWork?.SubModuleRepository.GetAll().ToList();
+    }
+
+    public List<SubModule> GetAll(Guid moduleId)
+    {
+      return _unitOfWork?.SubModuleRepository.GetAll(moduleId).ToList();
+    }
+    public SubModule? Get(string subModuleName)
+    {
+      return _unitOfWork?.SubModuleRepository.Get(s => s.SubModuleName.Equals(subModuleName));
+    }
+
+    public SubModule? GetById(Guid id)
+    {
+      return _unitOfWork?.SubModuleRepository.GetById(id);
+    }
+
+    public SubModule? GetByModuleId(Guid moduleId)
+    {
+      return _unitOfWork?.SubModuleRepository.GetByModuleId(moduleId);
+    }
+
+    public SubModule? GetByPermission(Guid id)
+    {
+      return _unitOfWork?.SubModuleRepository.GetByModuleId(id);
+    }
+
+    public void Dispose()
+    {
+      _unitOfWork?.Dispose();
+    }
+  }
+}
---
+++ backend/UsersAPI.Domain/Services/UserDomainService.cs
@@ -0,0 +1,85 @@
+using UsersAPI.Domain.Entities;
+using UsersAPI.Domain.Exceptions;
+using UsersAPI.Domain.Interfaces.Messages;
+using UsersAPI.Domain.Interfaces.Repositories;
+using UsersAPI.Domain.Interfaces.Services;
+using UsersAPI.Domain.ValueObjects;
+
+namespace UsersAPI.Domain.Services
+{
+  public class UserDomainService : IUserDomainService
+  {
+    private readonly IUnitOfWork? _unitOfWork;
+    private readonly IUserMessageProducer? userMessageProducer;
+
+    public UserDomainService(IUnitOfWork? unitOfWork, IUserMessageProducer? userMessageProducer)
+    {
+      _unitOfWork = unitOfWork;
+      this.userMessageProducer = userMessageProducer;
+    }
+
+    public void Add(User user)
+    {
+      if (Get(user.Email) != null)
+        throw new EmailAlreadyExistsException(user.Email);
+
+      if (_unitOfWork.RoleRepository.GetById(user.RoleId) == null)
+        throw new RoleIsNotExistsException(user.RoleId);
+
+      _unitOfWork?.UserRepository.Add(user);
+      _unitOfWork?.SaveChanges();
+
+      userMessageProducer?.Send(new UserMessageVO
+      {
+        Email = user.Email,
+        Subject = "Parabéns, sua conta de usuário foi criada com sucesso",
+        Body = @$"Olá {user.FirstName}, clique no link para ativar seu Usuário em nosso sistema."
+      });
+    }
+
+    public void Update(User user)
+    {
+      if (GetByRoleId(user.RoleId) != null)
+        throw new RoleIsNotExistsException(user.RoleId);
+
+      _unitOfWork?.UserRepository.Update(user);
+      _unitOfWork?.SaveChanges();
+    }
+
+    public void Delete(User user)
+    {
+      _unitOfWork?.UserRepository.Delete(user);
+      _unitOfWork?.SaveChanges();
+    }
+
+    public List<User> GetAll()
+    {
+      return _unitOfWork?.UserRepository.GetAll().ToList();
+    }
+
+    public User? Get(Guid id)
+    {
+      return _unitOfWork?.UserRepository.GetById(id);
+    }
+
+    public User? Get(string email)
+    {
+      return _unitOfWork?.UserRepository.Get(u => u.Email.Equals(email));
+    }
+
+    public User? Get(string email, string password)
+    {
+      return _unitOfWork?.UserRepository.Get(u => u.Email.Equals(email) && u.Password.Equals(password));
+    }
+
+    public User? GetByRoleId(Guid roleId)
+    {
+      return _unitOfWork?.UserRepository.Get(u => u.RoleId.Equals(roleId));
+    }
+
+    public void Dispose()
+    {
+      _unitOfWork?.Dispose();
+    }
+  }
+}
