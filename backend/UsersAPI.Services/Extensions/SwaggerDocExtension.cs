e7d49843fef41155d5ef1b2e7e006a8175adde2b
Álefe Alves <alefealves@hotmail.com>
2024-06-18 12:09:20 -0400

    feat: primeiras configurações e add de CorsPolicy e SwaggerDoc

---
+++ backend/UsersAPI.Services/Extensions/SwaggerDocExtension.cs
@@ -0,0 +1,43 @@
+namespace UsersAPI.Services.Extensions
+{
+    public static class SwaggerDocExtension
+    {
+        public static IServiceCollection AddSwaggerDoc(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                new OpenApiInfo
+                {
+                    Title = "UsersAPI",
+                    Description = "API para gerenciar usuários",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Álefe Alves",
+                        Email = "alefealves@hotmail.com"
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                options.IncludeXmlComments(xmlPath);
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDoc(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "UsersAPI");
+            });
+
+            return app;
+        }
+    }
+}
